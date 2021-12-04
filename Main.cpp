91d8c8c3e126f867e73ec91b856e3b5c648a7381
Kevin Allen <64329665+kevin-allen-uf@users.noreply.github.com>
2021-12-04 18:41:48 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,17 @@
+#include "Graph.h"
+#include "Menu.h"
+// Other includes go here
+using namespace std;
 
+int main () {
+    /* 
+       1. Load in graph dataset
+       2. Load menu prompt, continue to 3 if exit not selected
+       3. Get user input
+       4. Perform BST or Modified Spanning Tree algorithm
+       5. Output valuable results for given algorithm
+       6. Go back to step 2 
+    */
+
+    return 0;
+}
--- Menu.h
+++ Menu.h
@@ -1,1 +1,12 @@
+#pragma once
+// Includes go here
+using namspace std;
 
+class Menu {
+    public:
+        // Public Functions
+
+    private:
+        // Private variables and functions
+
+};
